6c4c5df7b95e0d11b88ccc16e5e2cac0d0a6c8a8
Liviu Ionescu <ilg@livius.net>
2021-03-27 23:01:52 +0200

    xpack.greeting command

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('xpack.helloWorld', () => {
+	let commandGreeting = vscode.commands.registerCommand('xpack.greeting', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from xPack C/C++ Build!');
+		vscode.window.showInformationMessage('Greetings from the xPack C/C++ Managed Build!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(commandGreeting);
 }
 
 // this method is called when your extension is deactivated
