c4db2face562abcdca9805b77a5752b2dba4f422
Liviu Ionescu <ilg@livius.net>
2021-03-30 23:31:55 +0300

    add code from vscode npm extension, not yet functional

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,161 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2021 Liviu Ionescu. All rights reserved.
+ *
+ * Licensed under the terms of the MIT License.
+ * See LICENSE in the project root for license information.
+ *
+ * This file is inspired by vscode.git/extensions/npm/src/npmView.ts.
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
+/* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
+
+// ----------------------------------------------------------------------------
+
+import * as assert from 'assert'
+
+// https://www.npmjs.com/package/request-light
+import * as httpRequest from 'request-light'
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
-// This method is called when your extension is activated
-export async function activate (context: vscode.ExtensionContext): Promise<void> {
+import { NpmScriptsTreeDataProvider } from './lib/tree-view-actions'
+import {
+  // getPackageManager, hasPackageJson
+  invalidateTasksCache,
+  NpmTaskProvider
+} from './lib/tasks'
+
+// ----------------------------------------------------------------------------
+
+let treeDataProvider: NpmScriptsTreeDataProvider | undefined
+
+export async function activate (
+  context: vscode.ExtensionContext): Promise<void> {
   console.log('"ilg-vscode.xpack" activated')
+
+  configureHttpRequest()
+  context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(
+    (e) => {
+      if (e.affectsConfiguration('http.proxy') ||
+        e.affectsConfiguration('http.proxyStrictSSL')) {
+        configureHttpRequest()
+      }
+    }))
+
+  // const canRunNPM = canRunNpmInCurrentWorkspace();
+  // context.subscriptions.push(addJSONProviders(httpRequest.xhr, canRunNPM));
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const commandGreeting = vscode.commands.registerCommand('xpack.greeting', async () => {
-    // The code you place here will be executed every time your command is executed
+  const commandGreeting = vscode.commands.registerCommand('xpack.greeting',
+    async () => {
+      // This code will be executed every time your command is executed
 
-    // Display a message box to the user
-    await vscode.window.showInformationMessage('Greetings from the xPack C/C++ Managed Build!')
-  })
+      // Display a message box to the user
+      await vscode.window.showInformationMessage(
+        'Greetings from the xPack C/C++ Managed Build!')
+    })
 
   context.subscriptions.push(commandGreeting)
+
+  registerTaskProvider(context)
+
+  treeDataProvider = registerExplorer(context)
+
+  context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((e) => {
+    if (e.affectsConfiguration('npm.exclude') ||
+    e.affectsConfiguration('npm.autoDetect')) {
+      invalidateTasksCache()
+      if (treeDataProvider != null) {
+        treeDataProvider.refresh()
+      }
+    }
+    if (e.affectsConfiguration('npm.scriptExplorerAction')) {
+      if (treeDataProvider != null) {
+        treeDataProvider.refresh()
+      }
+    }
+  }))
+
+  // registerHoverProvider(context);
+  // TODO: add the rest
+  console.log('"ilg-vscode.xpack" activation completed')
 }
 
-// this method is called when your extension is deactivated
 export function deactivate (): void {
   console.log('"ilg-vscode.xpack" deactivated')
 }
+
+// ----------------------------------------------------------------------------
+
+function invalidateScriptCaches (): void {
+  // invalidateHoverScriptsCache();
+  invalidateTasksCache()
+  if (treeDataProvider != null) {
+    treeDataProvider.refresh()
+  }
+}
+
+let taskProvider: NpmTaskProvider
+function registerTaskProvider (
+  context: vscode.ExtensionContext): vscode.Disposable | undefined {
+  if (vscode.workspace.workspaceFolders != null) {
+    const watcher = vscode.workspace.createFileSystemWatcher('**/package.json')
+    watcher.onDidChange((_e) => invalidateScriptCaches())
+    watcher.onDidDelete((_e) => invalidateScriptCaches())
+    watcher.onDidCreate((_e) => invalidateScriptCaches())
+    context.subscriptions.push(watcher)
+
+    const workspaceWatcher = vscode.workspace.onDidChangeWorkspaceFolders(
+      (_e) => {
+        invalidateScriptCaches()
+      }
+    )
+    context.subscriptions.push(workspaceWatcher)
+
+    taskProvider = new NpmTaskProvider(context)
+    const disposable = vscode.tasks.registerTaskProvider('xpack', taskProvider)
+    context.subscriptions.push(disposable)
+    return disposable
+  }
+  return undefined
+}
+
+function registerExplorer (
+  context: vscode.ExtensionContext): NpmScriptsTreeDataProvider | undefined {
+  if (vscode.workspace.workspaceFolders != null) {
+    assert(taskProvider)
+    const treeDataProvider =
+    new NpmScriptsTreeDataProvider(context, taskProvider)
+    const view = vscode.window.createTreeView('xpack',
+      { treeDataProvider: treeDataProvider, showCollapseAll: true })
+    context.subscriptions.push(view)
+    return treeDataProvider
+  }
+  return undefined
+}
+
+// function registerHoverProvider(context: vscode.ExtensionContext):
+// NpmScriptHoverProvider | undefined
+
+function configureHttpRequest (): void {
+  const httpSettings = vscode.workspace.getConfiguration('http')
+  httpRequest.configure(httpSettings.get<string>('proxy', ''),
+    httpSettings.get<boolean>('proxyStrictSSL', true))
+}
+
+// function canRunNpmInCurrentWorkspace (): boolean {
+//   if (vscode.workspace.workspaceFolders != null) {
+//     return vscode.workspace.workspaceFolders.some(
+//        (f) => f.uri.scheme === 'file'
+//     )
+//   }
+//   return false
+// }
+
+// ----------------------------------------------------------------------------
