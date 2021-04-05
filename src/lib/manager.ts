3c60e29438be88e468598a1babc506fbe61cf151
Liviu Ionescu <ilg@livius.net>
2021-04-05 23:22:09 +0300

    manager: rework to build tree & array

--- src/lib/manager.ts
+++ src/lib/manager.ts
@@ -35,12 +35,29 @@ export interface XpackFolderPath {
   packageJson: any
 }
 
+// This type reflects the properties of the `xpack` task definition
+// in `package.json`.
+// It must be filled in and passed as the first parameter
+// when creating tasks.
+// In adition to these members, an inherited `type` must be set
+// to `xPack`.
+export interface XpackTaskDefinition extends vscode.TaskDefinition {
+  actionName?: string
+  buildConfigurationName?: string
+  packageFolderRelativePath?: string
+}
+
+export type JsonActionValue = string | string[]
+
 // ----------------------------------------------------------------------------
 
 export class ExtensionManager {
   vscodeContext: vscode.ExtensionContext
   refreshFunctions: AyncVoidFunction[] = []
   xpackFolderPaths: XpackFolderPath[] = []
+
+  tasksTree: TreeNodePackage[] = []
+  tasks: vscode.Task[] = []
 
   constructor (context: vscode.ExtensionContext) {
     this.vscodeContext = context
@@ -63,6 +80,8 @@ export class ExtensionManager {
 
   async runRefreshFunctions (): Promise<void> {
     await this.findXpackFolderPaths()
+    await this.buildTasksTree()
+
     // No async forEach, use loop.
     for (const func of this.refreshFunctions) {
       await func()
@@ -142,6 +161,221 @@ export class ExtensionManager {
       'setContext',
       'xpack:showScriptExplorer',
       this.xpackFolderPaths.length >= 0)
+  }
+
+  // --------------------------------------------------------------------------
+
+  async buildTasksTree (): Promise<void> {
+    for (const xpackFolderPath of this.xpackFolderPaths) {
+      const treeNodePackage: TreeNodePackage =
+        new TreeNodePackage(xpackFolderPath)
+
+      this.tasksTree.push(treeNodePackage)
+
+      const packageJson = xpackFolderPath.packageJson
+
+      await this.addActions(packageJson.xpack.actions, treeNodePackage)
+
+      await this.addBuildConfigurations(
+        packageJson.xpack.buildConfigurations,
+        treeNodePackage)
+    }
+  }
+
+  async addActions (
+    fromJson: any,
+    parent: TreeNodeBuildConfiguration | TreeNodePackage
+  ): Promise<void> {
+    if (fromJson !== undefined) {
+      // Do not use Promise.all() to preserve JSON order.
+      for (const actionName of Object.keys(fromJson)) {
+        const actionValue: JsonActionValue = fromJson[actionName]
+        const nodeAction = await parent.addAction(actionName, actionValue, this)
+        // Also collect an array of tasks
+        this.tasks.push(nodeAction.task)
+      }
+    }
+  }
+
+  async addBuildConfigurations (
+    fromJson: any,
+    parent: TreeNodePackage
+  ): Promise<void> {
+    if (fromJson !== undefined) {
+      // Do not use Promise.all() to preserve JSON order.
+      for (const buildConfigurationName of Object.keys(fromJson)) {
+        const nodeBuildConfiguration =
+          await parent.addBuildConfiguration(buildConfigurationName)
+
+        const buildConfiguration: any = fromJson[buildConfigurationName]
+
+        await this.addActions(
+          buildConfiguration.actions,
+          nodeBuildConfiguration
+        )
+      }
+    }
+  }
+
+  // --------------------------------------------------------------------------
+
+  async createTaskAction (
+    actionName: string,
+    buildConfigurationName: string, // If empty, it is a package action.
+    xpackFolderPath: XpackFolderPath
+  ): Promise<vscode.Task> {
+    const taskDefinition: XpackTaskDefinition = {
+      type: 'xPack',
+      actionName
+    }
+
+    if (buildConfigurationName !== '') {
+      taskDefinition.buildConfigurationName = buildConfigurationName
+    }
+    if (xpackFolderPath.relativePath !== '') {
+      taskDefinition.packageFolderRelativePath =
+        xpackFolderPath.relativePath
+    }
+
+    const commandArguments = ['run', actionName]
+    let taskLabel = actionName
+    if (buildConfigurationName !== '') {
+      taskLabel += ` ${buildConfigurationName}`
+      commandArguments.push('--config', buildConfigurationName)
+    }
+    if (xpackFolderPath.relativePath !== '') {
+      taskLabel += ` (${xpackFolderPath.relativePath})`
+    }
+
+    const task = await this.createTask(
+      'xpm',
+      commandArguments,
+      xpackFolderPath,
+      taskLabel,
+      taskDefinition
+    )
+
+    return task
+  }
+
+  async createTask (
+    xpmProgramName: string,
+    commandArguments: string[],
+    xPackFolderPath: XpackFolderPath,
+    taskLabel: string,
+    taskDefinition: XpackTaskDefinition
+  ): Promise<vscode.Task> {
+    const scope: vscode.WorkspaceFolder = xPackFolderPath.workspaceFolder
+    const execution: vscode.ShellExecution = new vscode.ShellExecution(
+      xpmProgramName,
+      commandArguments,
+      { cwd: xPackFolderPath.path }
+    )
+
+    const taskPrefix = 'xPack'
+
+    const problemMatchers = undefined
+    const task = new vscode.Task(
+      taskDefinition,
+      scope,
+      taskLabel,
+      taskPrefix,
+      execution,
+      problemMatchers
+    )
+    task.detail = [xpmProgramName, ...commandArguments].join(' ')
+    return task
+  }
+
+  // --------------------------------------------------------------------------
+}
+
+// ----------------------------------------------------------------------------
+
+export class TreeNodePackage {
+  xpackFolderPath: XpackFolderPath
+  actions: TreeNodeAction[]
+  buildConfigurations: TreeNodeBuildConfiguration[]
+
+  constructor (xpackFolderPath: XpackFolderPath) {
+    this.xpackFolderPath = xpackFolderPath
+    this.actions = []
+    this.buildConfigurations = []
+  }
+
+  async addAction (
+    actionName: string,
+    actionValue: JsonActionValue,
+    extensionManager: ExtensionManager
+  ): Promise<TreeNodeAction> {
+    const task = await extensionManager.createTaskAction(
+      actionName,
+      '',
+      this.xpackFolderPath
+    )
+    const nodeAction = new TreeNodeAction(actionName, actionValue, task, this)
+    this.actions.push(nodeAction)
+
+    return nodeAction
+  }
+
+  addBuildConfiguration (
+    buildConfigurationName: string
+  ): TreeNodeBuildConfiguration {
+    const nodeBuildConfiguration = new TreeNodeBuildConfiguration(
+      buildConfigurationName,
+      this
+    )
+    this.buildConfigurations.push(nodeBuildConfiguration)
+
+    return nodeBuildConfiguration
+  }
+}
+
+export class TreeNodeBuildConfiguration {
+  buildConfigurationName: string
+  actions: TreeNodeAction[]
+  parent: TreeNodePackage
+
+  constructor (buildConfigurationName: string, parent: TreeNodePackage) {
+    this.buildConfigurationName = buildConfigurationName
+    this.actions = []
+    this.parent = parent
+  }
+
+  async addAction (
+    actionName: string,
+    actionValue: JsonActionValue,
+    extensionManager: ExtensionManager
+  ): Promise<TreeNodeAction> {
+    const task = await extensionManager.createTaskAction(
+      actionName,
+      this.buildConfigurationName,
+      this.parent.xpackFolderPath
+    )
+    const nodeAction = new TreeNodeAction(actionName, actionValue, task, this)
+    this.actions.push(nodeAction)
+
+    return nodeAction
+  }
+}
+
+export class TreeNodeAction {
+  actionName: string
+  actionValue: JsonActionValue
+  task: vscode.Task
+  parent: TreeNodeBuildConfiguration | TreeNodePackage
+
+  constructor (
+    actionName: string,
+    actionValue: JsonActionValue,
+    task: vscode.Task,
+    parent: TreeNodeBuildConfiguration | TreeNodePackage
+  ) {
+    this.actionName = actionName
+    this.actionValue = actionValue
+    this.task = task
+    this.parent = parent
   }
 }
 
