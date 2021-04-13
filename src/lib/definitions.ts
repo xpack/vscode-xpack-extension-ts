895da907531fdca46eeff77b7662bce037b6a611
Liviu Ionescu <ilg@livius.net>
2021-04-13 18:11:20 +0300

    add lib/definitions.ts

---
+++ src/lib/definitions.ts
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2021 Liviu Ionescu. All rights reserved.
+ *
+ * Licensed under the terms of the MIT License.
+ * See LICENSE in the project root for license information.
+ *
+ * This file was inspired by vscode.git/extensions/npm/src/*.ts.
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
+/* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
+
+// ----------------------------------------------------------------------------
+
+import * as vscode from 'vscode'
+
+// ----------------------------------------------------------------------------
+// Types.
+
+export type AsyncVoidFunction = (() => Promise<void>)
+
+export type JsonActionValue = string | string[]
+
+export interface JsonActions {
+  [actionName: string]: JsonActionValue
+}
+
+export interface JsonBuildConfiguration {
+  properties: string[]
+  actions: JsonActions
+}
+
+export interface JsonBuildConfigurations {
+  [buildConfigurationName: string]: JsonBuildConfiguration
+}
+
+export interface PackageJson {
+  name: string
+  version: string
+  xpack: {
+    properties: string[]
+    actions: JsonActions
+    buildConfigurations: JsonBuildConfigurations
+  }
+}
+
+/**
+ * This type reflects the properties of the `xpack` task definition
+ * in `package.json`.
+ * It must be filled in and passed as the first parameter
+ * when creating tasks.
+ * In adition to these members, an inherited property named `type` must
+ * be set to `xPack`.
+ */
+export interface XpackTaskDefinition extends vscode.TaskDefinition {
+  actionName?: string
+  buildConfigurationName?: string
+  packageFolderRelativePath?: string
+  xpmCommand?: string
+}
+
+// ----------------------------------------------------------------------------
+// Constants.
+
+export const packageJsonFileName: string = 'package.json'
+
+// ----------------------------------------------------------------------------
