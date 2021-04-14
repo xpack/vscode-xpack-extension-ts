2cd8566ab0c832351f9a38b078751505b9921791
Liviu Ionescu <ilg@livius.net>
2021-04-14 18:12:50 +0300

    utils: remove filterPath, moved to xpm-liquid

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,7 +13,6 @@
 
 // ----------------------------------------------------------------------------
 
-import * as os from 'os'
 import * as path from 'path'
 
 import * as vscode from 'vscode'
@@ -114,17 +113,6 @@ export function isPrimitive (value: any): boolean {
 
 export function isJsonObject (value: any): boolean {
   return value !== undefined && !isPrimitive(value) && !Array.isArray(value)
-}
-
-// ----------------------------------------------------------------------------
-
-export function filterPath (input: string): string {
-  const lower = input.toLowerCase()
-  const fixed = (os.platform() === 'win32')
-    ? lower.replace(/[^a-z0-9\\]+/g, '-')
-    : lower.replace(/[^a-z0-9/]+/g, '-')
-
-  return fixed.replace(/--/g, '-')
 }
 
 // ----------------------------------------------------------------------------
